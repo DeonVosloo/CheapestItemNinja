c258e6a90f0c8fbdc0ff41cdef58f86588176d0c
DeonVosloo <deonvosloo3366@gmail.com>
2024-01-25 14:52:19 +0200

    basic test Stream builder implemented and functioning to display test data.

--- lib/pages/TestPage.dart
+++ lib/pages/TestPage.dart
@@ -18,69 +18,26 @@ class _TestingPageState extends State<TestingPage> {
   //List<Row> testList = [];
   @override
   Widget build(BuildContext context) {
-     var db = FirebaseFirestore.instance;
-    // final docRef = db.collection("test").doc("8dPYnFecIBSQqgWSJHDU");
-    // docRef.snapshots().listen(
-    //       (event) => print("current data: ${event.data()}"),
-    //   onError: (error) => print("Listen failed: $error"),
-    // );
+    final Stream<QuerySnapshot> citiesStream =
+    FirebaseFirestore.instance.collection('cities').snapshots();
+
+    var db = FirebaseFirestore.instance;
 
     final cities = db.collection('cities');
-     final data1 = <String, dynamic>{
-       "name": "San Francisco",
-       "state": "CA",
-       "country": "USA",
-       "capital": false,
-       "population": 860000,
-       "regions": ["west_coast", "norcal"]
-     };
-     cities.doc("SF").set(data1);
-
-     final data2 = <String, dynamic>{
-       "name": "Los Angeles",
-       "state": "CA",
-       "country": "USA",
-       "capital": false,
-       "population": 3900000,
-       "regions": ["west_coast", "socal"],
-     };
-     cities.doc("LA").set(data2);
-
-     final data3 = <String, dynamic>{
-       "name": "Washington D.C.",
-       "state": null,
-       "country": "USA",
-       "capital": true,
-       "population": 680000,
-       "regions": ["east_coast"]
-     };
-     cities.doc("DC").set(data3);
-
-     final data4 = <String, dynamic>{
-       "name": "Tokyo",
-       "state": null,
-       "country": "Japan",
-       "capital": true,
-       "population": 9000000,
-       "regions": ["kanto", "honshu"]
-     };
-     cities.doc("TOK").set(data4);
-
-     final data5 = <String, dynamic>{
-       "name": "Beijing",
-       "state": null,
-       "country": "China",
-       "capital": true,
-       "population": 21500000,
-       "regions": ["jingjinji", "hebei"],
-     };
-     cities.doc("BJ").set(data5);
+    final data1 = <String, dynamic>{
+      "name": "San Francisco",
+      "state": "CA",
+      "country": "USA",
+      "capital": false,
+      "population": 860000,
+      "regions": ["west_coast", "norcal"]
+    };
+    cities.doc("VSKGTEA6X1UOpEJFY0YZ").set(data1);
 
 
     return Scaffold
       (
-      appBar: AppBar(title: Text("Testing Firebase"),
-      backgroundColor: Colors.blueAccent,),
+      appBar: AppBar(title: Text("Testing Firebase"), backgroundColor: Colors.blueAccent,),
       body: ListView
         (
         children: [
@@ -92,52 +49,43 @@ class _TestingPageState extends State<TestingPage> {
 
           ElevatedButton(onPressed: ()
           {
-            final docRef = db.collection("cities").doc("VSKGTEA6X1UOpEJFY0YZ ");
-            docRef.get().then(
-                  (DocumentSnapshot doc) {
-                final data = doc.data() as Map<String, dynamic>;
-                // ...
-              },
-              onError: (e) => print("Error getting document: $e"),
-            );
+            // CollectionReference collReff = FirebaseFirestore.instance.collection('test');
+            // collReff.add(
+            //     {
+            //       'testing': _textController1.text,
+            //     }
+            // );
           },
-              child: const Text("Add test data"))
+              child: const Text("Add test data")),
 
 
+      StreamBuilder<QuerySnapshot>(
+        stream: citiesStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
 
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
 
-          // StreamBuilder<QuerySnapshot>
-          //   (
-          //     stream: FirebaseFirestore.instance.collection('test').snapshots(),
-          //     builder: (context,snapshot)
-          //     {
-          //       List<Row> clientWidgets =[];
-          //
-          //       if(snapshot.hasData)
-          //         {
-          //           final clients = snapshot.data?.docs.reversed.toList();
-          //           for(var client in clients!)
-          //             {
-          //               final clientWidget = Row
-          //                 (
-          //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //                 children: [
-          //                   Text(client['testing'])
-          //                 ],
-          //               );
-          //               clientWidgets.add(clientWidget);
-          //             }
-          //         }
-          //
-          //       return Expanded
-          //         (
-          //           child: ListView
-          //             (
-          //             children: clientWidgets,
-          //           )
-          //       );
-          //     }
-          // )
+          return ListView(
+            shrinkWrap: true,
+            children: snapshot.data!.docs
+                .map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+              document.data()! as Map<String, dynamic>;
+              return ListTile(
+                title: Text(data['name']),
+                subtitle: Text(data['country']),
+              );
+            })
+                .toList()
+                .cast(),
+          );
+        },
+      ),
         ],
       ),
     );
